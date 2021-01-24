eb9789b32bb400903f9a45ac09342da3a4f2da6e
Sick-codes <71471319+Sick-codes@users.noreply.github.com>
2021-01-24 14:51:46 -0600

    Update user.py

--- core/authentication/user.py
+++ core/authentication/user.py
@@ -7,17 +7,7 @@ conn = pymongo.MongoClient("localhost", 27017)
 db = conn.get_database("users")
 col = db.get_collection("user")
 
-class Main(object):
-    async def create_user(self, email ,user, pasw):
-        col.insert_one({
-            "email":email,
-            "username":user,
-            "passwd":pasw,
-            #this might not work as intended but if not i will fix it
-            "created_at":datetime.now(),
-            "last_login":datetime.now()
-        }) 
-    
+class Main(object):    
 
     async def hashPass(self, passwd) -> bytes:
         return hashpw(str(passwd).encode('utf8'), gensalt())
@@ -27,5 +17,17 @@ class Main(object):
         psw = col.find_one({'username':user})['passwd']
         return checkpw(str(passwd).encode('utf8'), psw)
 
-    
+
+    async def create_user(self, email ,user, pasw):
+        col.insert_one({
+            "email":email,
+            "username":user,
+            "passwd":r(self.hashPass(pasw)),
+            #this might not work as intended but if not i will fix it
+            "created_at":datetime.now(),
+            "last_login":datetime.now()
+        }) 
+
+
+inst = Main()
 

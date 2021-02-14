be3f0af6c4823d9fe0133f7f20bc94ebb66a2984
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2021-02-14 09:41:51 +0100

    adjust config/data dir for tests

--- tests/addons/api/test_init.py
+++ tests/addons/api/test_init.py
@@ -1,6 +1,5 @@
 """Test the default api implementation."""
 import os
-from pathlib import Path
 
 import pytest
 
@@ -8,12 +7,12 @@ from core.core import ApplicationCore, CoreState
 
 
 @pytest.mark.asyncio
-async def test_api_get_non_existing_state(aiohttp_client, loop):
+async def test_api_get_non_existing_state(aiohttp_client, loop, tmp_path):
     """Test if the debug interface allows us to get a state."""
     core = ApplicationCore()
     core.config.internal_url = ""
     core.config.external_url = ""
-    core.config.data_dir = os.path.join(Path.cwd(), "tests/resources", "data")
-    core.config.config_dir = os.path.join(Path.cwd(), "tests/resources", "config")
+    core.config.data_dir = os.path.join(tmp_path, "data")
+    core.config.config_dir = os.path.join(tmp_path, "config")
     core.state = CoreState.running
     await core.start()

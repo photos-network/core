0f86dae731d2789412c3888804fe0ab3e07e837d
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2021-02-17 19:18:19 +0100

    create data dir for tests

--- tests/addons/api/test_init.py
+++ tests/addons/api/test_init.py
@@ -14,6 +14,7 @@ async def test_api_get_non_existing_state(aiohttp_client, loop, tmp_path):
     core.config.internal_url = ""
     core.config.external_url = ""
     core.config.data_dir = os.path.join(tmp_path, "data")
+    core.config.data_dir.mkdir()
     core.config.config_dir = os.path.join(Path.cwd(), "tests/resources", "config")
     core.state = CoreState.running
     await core.start()
